faba042e97eec506964dee24c1b10322cd1a3619
deloue <delouejose@gmail.com>
2025-08-04 13:28:55 -0400

    added advanced search engine

--- src/index.js
+++ src/index.js
@@ -5,19 +5,22 @@ function refreshweather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  let date = new Date(response.data.dt * 1000);
   let temperature = response.data.main.temp;
   let city = response.data.name;
   let description = response.data.weather[0].description;
   let humidity = response.data.main.humidity;
   let windSpeed = response.data.wind.speed;
+  let iconCode = response.data.weather[0].icon;
 
   cityElement.innerHTML = city;
   humidityElement.innerHTML = `${humidity}%`;
   windSpeedElement.innerHTML = `${windSpeed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   timeElement.innerHTML = `${formateDate(date)}, ${description}`;
+  iconElement.innerHTML = `<img src="https://openweathermap.org/img/wn/${iconCode}@2x.png" class="weather-app-icon" />`;
 }
 
 function formateDate(date) {
@@ -38,11 +41,13 @@ function formateDate(date) {
   }
   return `${day} ${hours}:${minutes}`;
 }
+
 function searchCity(city) {
   let apiKey = "ff6e78b219e243b3f72eaf0251bd1e56";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshweather);
 }
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
